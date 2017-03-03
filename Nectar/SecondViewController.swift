a836db9cfa41f0aa8a1c4f5679369dcfb153cfda
Jordan White <jordanrw@vt.edu>
2017-03-02 20:54:32 -0500

    Initial commit

---
+++ Nectar/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Nectar
+//
+//  Created by Jordan White on 3/2/17.
+//  Copyright © 2017 Two Beards and Fro. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
